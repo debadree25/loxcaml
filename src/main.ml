143268cb7aa5c62e7c4b87fdf90e1e1e558d69dd
Debadree Chatterjee <debadree333@gmail.com>
2024-12-30 12:30:45 +0530

    fix advancement logic and refactoring

--- src/main.ml
+++ src/main.ml
@@ -19,8 +19,8 @@ type token =
   | LESS
   | LESS_EQUAL
   | IDENTIFIER of string
-  | STRING
-  | NUMBER
+  | STRING of string
+  | NUMBER of float
   | AND
   | CLASS
   | ELSE
@@ -61,8 +61,8 @@ let token_type_to_name_str t =
   | LESS -> "LESS"
   | LESS_EQUAL -> "LESS_EQUAL"
   | IDENTIFIER s -> "IDENTIFIER " ^ s
-  | STRING -> "STRING"
-  | NUMBER -> "NUMBER"
+  | STRING s -> "STRING " ^ s
+  | NUMBER n -> "NUMBER " ^ string_of_float n
   | AND -> "AND"
   | CLASS -> "CLASS"
   | ELSE -> "ELSE"
@@ -81,6 +81,48 @@ let token_type_to_name_str t =
   | WHILE -> "WHILE"
   | EOF -> "EOF"
 
+let token_type_to_str t =
+  match t with
+  | LEFT_PAREN -> "("
+  | RIGHT_PAREN -> ")"
+  | LEFT_BRACE -> "{"
+  | RIGHT_BRACE -> "}"
+  | COMMA -> ","
+  | DOT -> "."
+  | MINUS -> "-"
+  | PLUS -> "+"
+  | SEMICOLON -> ";"
+  | SLASH -> "/"
+  | STAR -> "*"
+  | BANG -> "!"
+  | BANG_EQUAL -> "!="
+  | EQUAL -> "="
+  | EQUAL_EQUAL -> "=="
+  | GREATER -> ">"
+  | GREATER_EQUAL -> ">="
+  | LESS -> "<"
+  | LESS_EQUAL -> "<="
+  | IDENTIFIER s -> s
+  | STRING s -> s
+  | NUMBER n -> string_of_float n
+  | AND -> "and"
+  | CLASS -> "class"
+  | ELSE -> "else"
+  | FALSE -> "false"
+  | FUN -> "fun"
+  | FOR -> "for"
+  | IF -> "if"
+  | NIL -> "nil"
+  | OR -> "or"
+  | PRINT -> "print"
+  | RETURN -> "return"
+  | SUPER -> "super"
+  | THIS -> "this"
+  | TRUE -> "true"
+  | VAR -> "var"
+  | WHILE -> "while"
+  | EOF -> ""
+
 type token_info = {
   ttype : token;
   lexeme : string;
@@ -93,6 +135,10 @@ let token_info_to_str (t : token_info) =
     (token_type_to_name_str t.ttype)
     t.lexeme
     (match t.literal with None -> "null" | Some s -> s)
+
+let make_token_info ttype literal line =
+  let lexeme = token_type_to_str ttype in
+  { ttype; lexeme; literal; line }
 
 type scanner = {
   source : string;
@@ -114,11 +160,22 @@ let make_scanner source =
   }
 
 let is_at_end scanner = scanner.current_lexeme >= String.length scanner.source
-let add_token scanner (ti : token_info) = scanner.tokens <- ti :: scanner.tokens
+
+let add_token scanner token literal line =
+  scanner.tokens <- make_token_info token literal line :: scanner.tokens
 
 let advance scanner =
   scanner.current_lexeme <- scanner.current_lexeme + 1;
   scanner.source.[scanner.current_lexeme - 1]
+
+let peek scanner =
+  if is_at_end scanner then '\000' else scanner.source.[scanner.current_lexeme]
+
+let match_next (scanner : scanner) expected =
+  if peek scanner <> expected then false
+  else (
+    ignore (advance scanner);
+    true)
 
 let advance_line scanner = scanner.line <- scanner.line + 1
 
@@ -129,68 +186,32 @@ let report_error scanner line message =
 let scan_token scanner =
   let c = advance scanner in
   match c with
-  | '(' ->
-      add_token scanner
-        {
-          ttype = LEFT_PAREN;
-          lexeme = "(";
-          literal = None;
-          line = scanner.line;
-        }
-  | ')' ->
-      add_token scanner
-        {
-          ttype = RIGHT_PAREN;
-          lexeme = ")";
-          literal = None;
-          line = scanner.line;
-        }
-  | '{' ->
-      add_token scanner
-        {
-          ttype = LEFT_BRACE;
-          lexeme = "{";
-          literal = None;
-          line = scanner.line;
-        }
-  | '}' ->
-      add_token scanner
-        {
-          ttype = RIGHT_BRACE;
-          lexeme = "}";
-          literal = None;
-          line = scanner.line;
-        }
-  | ',' ->
-      add_token scanner
-        { ttype = COMMA; lexeme = ","; literal = None; line = scanner.line }
-  | '.' ->
-      add_token scanner
-        { ttype = DOT; lexeme = "."; literal = None; line = scanner.line }
-  | '-' ->
-      add_token scanner
-        { ttype = MINUS; lexeme = "-"; literal = None; line = scanner.line }
-  | '+' ->
-      add_token scanner
-        { ttype = PLUS; lexeme = "+"; literal = None; line = scanner.line }
-  | ';' ->
-      add_token scanner
-        { ttype = SEMICOLON; lexeme = ";"; literal = None; line = scanner.line }
-  | '*' ->
-      add_token scanner
-        { ttype = STAR; lexeme = "*"; literal = None; line = scanner.line }
+  | '(' -> add_token scanner LEFT_PAREN None scanner.line
+  | ')' -> add_token scanner RIGHT_PAREN None scanner.line
+  | '{' -> add_token scanner LEFT_BRACE None scanner.line
+  | '}' -> add_token scanner RIGHT_BRACE None scanner.line
+  | ',' -> add_token scanner COMMA None scanner.line
+  | '.' -> add_token scanner DOT None scanner.line
+  | '-' -> add_token scanner MINUS None scanner.line
+  | '+' -> add_token scanner PLUS None scanner.line
+  | ';' -> add_token scanner SEMICOLON None scanner.line
+  | '*' -> add_token scanner STAR None scanner.line
   | '!' ->
-      add_token scanner
-        { ttype = BANG; lexeme = "!"; literal = None; line = scanner.line }
+      if match_next scanner '=' then
+        add_token scanner BANG_EQUAL None scanner.line
+      else add_token scanner BANG None scanner.line
   | '=' ->
-      add_token scanner
-        { ttype = EQUAL; lexeme = "="; literal = None; line = scanner.line }
+      if match_next scanner '=' then
+        add_token scanner EQUAL_EQUAL None scanner.line
+      else add_token scanner EQUAL None scanner.line
   | '<' ->
-      add_token scanner
-        { ttype = LESS; lexeme = "<"; literal = None; line = scanner.line }
+      if match_next scanner '=' then
+        add_token scanner LESS_EQUAL None scanner.line
+      else add_token scanner LESS None scanner.line
   | '>' ->
-      add_token scanner
-        { ttype = GREATER; lexeme = ">"; literal = None; line = scanner.line }
+      if match_next scanner '=' then
+        add_token scanner GREATER_EQUAL None scanner.line
+      else add_token scanner GREATER None scanner.line
   | '\n' -> advance_line scanner
   | _ ->
       report_error scanner scanner.line
@@ -201,8 +222,7 @@ let scan_tokens scanner =
     scanner.start_lexeme <- scanner.current_lexeme;
     scan_token scanner
   done;
-  add_token scanner
-    { ttype = EOF; lexeme = ""; literal = None; line = scanner.line };
+  add_token scanner EOF None scanner.line;
   List.rev scanner.tokens
 
 type tokenize_result = { tokens : token_info list; had_error : bool }
@@ -232,6 +252,4 @@ let () =
       (fun t -> print_endline (token_info_to_str t))
       tokenize_result.tokens;
     if tokenize_result.had_error then exit 65 else ())
-  else
-    print_endline
-      (token_info_to_str { ttype = EOF; lexeme = ""; literal = None; line = 1 })
+  else print_endline (token_info_to_str (make_token_info EOF None 1))
