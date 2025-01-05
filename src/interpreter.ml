519c2899b6d35e4f0fa1515b888227bcf232fbf7
Debadree Chatterjee <debadree333@gmail.com>
2025-01-05 21:16:54 +0530

    handle grouping expr

--- src/interpreter.ml
+++ src/interpreter.ml
@@ -9,7 +9,8 @@ let interpret_literal_to_str = function
       if Float.is_integer n then Printf.sprintf "%d" (int_of_float n)
       else Printf.sprintf "%.15g" n
 
-let interpreter (expr : expr) =
+let rec interpreter expr =
   match expr with
   | Literal lit -> Ok (interpret_literal_to_str lit)
+  | Grouping expr -> interpreter expr
   | _ -> Error "Not implemented yet"
