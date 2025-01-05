a38da7f8bc6e10e10ad58faa0788c6d44ecb280e
Debadree Chatterjee <debadree333@gmail.com>
2025-01-05 21:14:47 +0530

    introduce special interpreter version of lit to string to appease tester

--- src/interpreter.ml
+++ src/interpreter.ml
@@ -1,7 +1,15 @@
 open Ast
 open Token
 
-let interpreter (expr: expr) =
+let interpret_literal_to_str = function
+  | LBool b -> string_of_bool b
+  | LNil -> "nil"
+  | LString s -> s
+  | LNumber n ->
+      if Float.is_integer n then Printf.sprintf "%d" (int_of_float n)
+      else Printf.sprintf "%.15g" n
+
+let interpreter (expr : expr) =
   match expr with
-  | Literal lit -> Ok (literal_to_str lit)
+  | Literal lit -> Ok (interpret_literal_to_str lit)
   | _ -> Error "Not implemented yet"
